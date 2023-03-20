cebccd086a2aba6ca2ea79bbab6fe4ec0d416212
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-20 18:13:38 -0500

    move getListings functionality into angular service

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { fakeListings } from '../fake-data';
+import { ListingsService } from '../listings.service';
 import { Listing } from '../types';
 
 @Component({
@@ -11,9 +11,11 @@ export class ListingsPageComponent implements OnInit {
 
     listings: Listing[] = [];
 
-    constructor() { }
+    constructor(
+        private listingsService: ListingsService
+    ) { }
 
     ngOnInit(): void {
-        this.listings = fakeListings;
+        this.listings = this.listingsService.getListings();
     }
 }
