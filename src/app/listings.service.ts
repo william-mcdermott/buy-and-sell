44fd1e89700e46ad3f1cb4a9c36ddb381bc28496
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-21 17:59:15 -0500

    Add auth functionality to create new listing

--- src/app/listings.service.ts
+++ src/app/listings.service.ts
@@ -65,11 +65,17 @@ export class ListingsService {
     }
 
     createListing(name: string, description: string, price: number): Observable<Listing> {
-        return this.http.post<Listing>(
-            '/api/listings',
-            { name, description, price },
-            httpOptions
-        )
+        return new Observable<Listing>(observer => {
+            this.auth.user.subscribe(user => {
+                user && user.getIdToken().then(token => {
+                    this.http.post<Listing>(
+                        '/api/listings',
+                        { name, description, price },
+                        httpOptionsWithAuthToken(token)
+                    ).subscribe(() => observer.next());
+                })
+            })
+        })
     }
 
     editListing(id: string, name: string, description: string, price: number): Observable<Listing> {
