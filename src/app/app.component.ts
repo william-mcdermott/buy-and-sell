a3ef40adfc358ffb5975b08af9d8ac2bf2e55409
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-21 17:31:17 -0500

    Sign in and out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'buy-and-sell';
+    title = 'buy-and-sell';
+
+    constructor(
+        public auth: AngularFireAuth
+    ) { }
+
+    signInClicked(): void {
+        this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    }
+
+    signOutClicked(): void {
+        this.auth.signOut();
+    }
 }
