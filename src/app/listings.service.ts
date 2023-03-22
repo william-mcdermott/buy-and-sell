3ee5b9e94166c4633164cc68b05bbda0969bc12e
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-22 17:49:53 -0500

    Add auth to delete listings functionality

--- src/app/listings.service.ts
+++ src/app/listings.service.ts
@@ -61,7 +61,14 @@ export class ListingsService {
     }
 
     deleteListing(id: string): Observable<any> {
-        return this.http.delete(`/api/listings/${id}`);
+        return new Observable<any>(observer => {
+            this.auth.user.subscribe(user => {
+                user && user.getIdToken().then(token => {
+                    this.http.delete(`/api/listings/${id}`, httpOptionsWithAuthToken(token))
+                        .subscribe(() => observer.next());
+                })
+            })
+        })
     }
 
     createListing(name: string, description: string, price: number): Observable<Listing> {
